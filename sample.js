1225b9ca001402b12a21a2369a82385e6ccd70f3
ranalini <renalinir007@gmail.com>
2026-06-16 11:15:00 +0530

    JS

---
+++ sample.js
@@ -0,0 +1,70 @@
+// console.log("A")
+// console.log("B")
+// console.log("C")
+
+// const promise =new promise((resolve,reject)=>{
+
+//     const success= true;
+//  if (success){
+//     console.log("Done")
+//  }
+//  else{
+//     console.log("NO")
+//  }
+// })
+//  promise.then(res=>{console.log(res)})
+//  .catch(err=>{console.log(err)})
+
+
+// console.log("A")
+// console.log("B")
+// console.log("C")
+
+// const promise =new Promise((resolve,reject)=>{
+//   setTimeout(()=>{
+//     console.log("Done")
+//   },0)
+//   console.log("D")
+// })
+//  promise.then(res=>{console.log(res)})
+//  .catch(err=>{console.log(err)})
+
+
+// console.log("A");
+// console.log("B");
+// console.log("C");
+
+// const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("Hi");
+//         resolve("Done");
+//     }, 2000);
+
+//     console.log("D");
+// });
+
+// promise
+//     .then(res => {
+//         console.log(res);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
+
+
+
+ console.log(1)
+    console.log(2)
+    console.log("Hi")
+    function example(){
+        console.log("Hello")
+    }
+    const example1=()=>{
+        const a=1;
+        const b=2;
+        console.log(1+2);
+    }
+
+    setTimeout(example(),30000)
+    setTimeout(example1(),450)
+
